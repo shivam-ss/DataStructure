40f8a8f91b79f6608ed412e2e46255de38aca5d1
shivam-ss <shivamsrivastava800@gmail.com>
2020-05-04 21:38:25 +0530

    matrix 2d

---
+++ matrix_2D.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure;
+
+/**
+ *
+ * @author shivam
+ */
+public class matrix_2D {
+    public static void main(String[] args){
+        int row=2;
+        int col=2;
+        int[][] m1= new int[row][col];
+        int[][] m2=new int[row][col];
+                      
+    }
+}
