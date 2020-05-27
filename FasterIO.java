19e86e135b10ac5e98baf7b13967b3cf67ca8bed
shivam-ss <shivamsrivastava800@gmail.com>
2020-05-27 21:47:46 +0530

    Merge sort v1

--- FasterIO.java
+++ FasterIO.java
@@ -9,6 +9,33 @@ package datastructure;
  *
  * @author shiva
  */
-public class FasterIO {
-    
-}
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.StringTokenizer; 
+  
+public class FasterIO 
+{ 
+    public static void main(String[] args) throws IOException 
+    { 
+  
+        BufferedReader br = new BufferedReader( 
+                              new InputStreamReader(System.in)); 
+  
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        int n = Integer.parseInt(st.nextToken()); 
+        int k = Integer.parseInt(st.nextToken()); 
+        int count = 0; 
+        while (n-- > 0) 
+        { 
+            int x = Integer.parseInt(br.readLine()); 
+            if (x%k == 0) 
+               count++; 
+        } 
+        System.out.println(count); 
+    } 
+} 
+
+// FROM GEEKSFORGEEKS, will be modifying and using it for my most recent codes
+
+
---
+++ MergeSort.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure;
+
+/**
+ *
+ * @author shiva
+ */
+public class MergeSort {
+    //breaking 
+    public static void mergeSort(int[] a, int n) {
+    if (n < 2) {
+        return;
+    }
+    int mid = n / 2;
+    int[] l = new int[mid];
+    int[] r = new int[n - mid];
+ 
+    for (int i = 0; i < mid; i++) {
+        l[i] = a[i];
+    }
+    for (int i = mid; i < n; i++) {
+        r[i - mid] = a[i];
+    }
+    mergeSort(l, mid);
+    mergeSort(r, n - mid);
+ 
+    merge(a, l, r, mid, n - mid);
+    
+    for(int i=0;i<n;i++){
+        
+    }
+}
+    
+    //merging
+    
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+  
+    int i = 0, j = 0, k = 0;
+    while (i < left && j < right) {
+        if (l[i] <= r[j]) {
+            a[k++] = l[i++];
+        }
+        else {
+            a[k++] = r[j++];
+        }
+    }
+    while (i < left) {
+        a[k++] = l[i++];
+    }
+    while (j < right) {
+        a[k++] = r[j++];
+    }
+}
+    public static void main(String[] args){
+        int[] arr = {5,1,6,2,3,4};
+        MergeSort obj=new MergeSort();
+        obj.mergeSort(arr,6);
+        
+    }
+    
+}
