f4f22819db73da5d5d6324671c4ada7c0771f435
shivam-ss <shivamsrivastava800@gmail.com>
2020-05-29 22:19:14 +0530

    Inorder traversal on Binary Tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -7,8 +7,86 @@ package datastructure;
 
 /**
  *
- * @author shiva
+ * @author shivam
  */
 public class BinarySearchTree {
     
+    
+    Node root; 
+    //class node inside BST class
+    class Node{
+        int key;
+        Node left;
+        Node right;
+    
+        public Node(int key){
+            key=key;
+            left=null;
+            right=null;
+        }
+    }
+        
+        //creating root of BST:
+    
+            BinarySearchTree(){
+            root=null; //
+              }
+            
+        void insert(int key){
+            root=insertRecord(root,key);
+        }
+        
+        Node insertRecord(Node root, int key){
+            //Returning a newly created Node if tree is empty
+            if(root==null){
+                root=new Node(key);
+                return root; 
+            }
+            if(key<root.key){
+                root.left=insertRecord(root.left,key);
+               
+            }
+            else if(key>root.key)
+                root.right=insertRecord(root.right,key);
+            
+            
+            return root;
+        }
+        void inorder(){
+            inorderRecord(root);
+            
+        }
+        void inorderRecord(Node root){
+            if(root!=null){
+                inorderRecord(root.left);
+                System.out.println(root.key);
+                inorderRecord(root.right);
+                
+            }
+        }
+        
+        
+        
+        public static void main(String[] args){
+            BinarySearchTree obj=new BinarySearchTree();
+            obj.insert(50);
+            obj.insert(10);
+            obj.insert(60);
+            obj.insert(45);
+            obj.insert(52);
+            obj.insert(11);
+            obj.insert(32);
+            obj.insert(78);
+            obj.insert(100);
+            obj.insert(200);
+            obj.insert(2);
+            obj.insert(99);
+            
+            
+            obj.inorder();
+        }
+        
+        
+        
+            
 }
