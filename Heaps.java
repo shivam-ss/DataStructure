ef600534d2365df6508ecea8c37e3974fb737e03
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-06-14 20:08:53 +0530

    Create Heaps.java

---
+++ Heaps.java
@@ -0,0 +1,4 @@
+Heaps are binary trees in which insertion happens at end node, then it is compared to parent node to check condition for min heap or max heap.
+In min heap, parent is always lesser than child and vice versa
+Deletion in heap happens at root level, then swapping and comparision starts
+Heaps only provide insertion/deletion at max or min element
